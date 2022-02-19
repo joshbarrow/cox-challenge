da8270b5f5015bd157dab25cbc86129e1615084f
Josh <joshrbarrow@gmail.com>
2022-02-18 18:25:37 -0800

    Vehicle.show

--- src/dealer-vehicle-api/vehicle.js
+++ src/dealer-vehicle-api/vehicle.js
@@ -1,11 +1,18 @@
 import axios from "axios";
-import DatasetApi from './dataset'
+import DatasetApi from "./dataset";
 
 export default class VehicleApi {
-  static async getVehicleIds(datasetId) {
+  static async index(datasetId) {
     const response = await axios.get(
       `http://api.coxauto-interview.com/api/${datasetId}/vehicles`
     );
     return response.data.vehicleIds;
+  }
+
+  static async show(datasetId, vehicleId) {
+    const response = await axios.get(
+      `http://api.coxauto-interview.com/api/${datasetId}/vehicles/${vehicleId}`
+    );
+    return response.data;
   }
 }
--- src/spec/vehicle.spec.js
+++ src/spec/vehicle.spec.js
@@ -2,13 +2,27 @@ import VehicleApi from "../dealer-vehicle-api/vehicle";
 import DatasetApi from "../dealer-vehicle-api/dataset";
 
 describe("VehicleApi", () => {
-  describe(".getVehicleIds", () => {
+  describe(".index", () => {
     it("returns an array of vehicle ids", async () => {
-      const datasetId = await DatasetApi.getId()
-      const vehicleIds = await VehicleApi.getVehicleIds(datasetId);
-      expect(Array.isArray(vehicleIds)).toBe(true)
-      expect(vehicleIds.length).toBe(9)
-      expect(vehicleIds.find((id) => typeof id !== "number" )).toBeUndefined()
+      const datasetId = await DatasetApi.getId();
+      const vehicleIds = await VehicleApi.index(datasetId);
+      expect(Array.isArray(vehicleIds)).toBe(true);
+      expect(vehicleIds.length).toBe(9);
+      expect(vehicleIds.find((id) => typeof id !== "number")).toBeUndefined();
+    });
+  });
+
+  describe(".show", () => {
+    it("returns a vehicle object", async () => {
+      const datasetId = await DatasetApi.getId();
+      const vehicleIds = await VehicleApi.index(datasetId);
+      const vehicleId = vehicleIds[0];
+      const vehicle = await VehicleApi.show(datasetId, vehicleId);
+      console.log(vehicle);
+      expect(typeof vehicle.dealerId).toBe("number");
+      expect(typeof vehicle.model).toBe("string");
+      expect(typeof vehicle.make).toBe("string");
+      expect(typeof vehicle.year).toBe("number");
     });
   });
 });
