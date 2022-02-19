96bc65560742cef12e68f825fb391316e514f8fc
Josh <joshrbarrow@gmail.com>
2022-02-18 21:37:20 -0800

    Dataset.inventory

--- src/dealer-vehicle-api/dataset.js
+++ src/dealer-vehicle-api/dataset.js
@@ -1,10 +1,52 @@
 import axios from "axios";
+import DealerApi from "./dealer";
+import VehicleApi from "./vehicle";
 
 export default class DatasetApi {
   static async getId() {
     const response = await axios.get(
       "http://api.coxauto-interview.com/api/datasetId"
     );
-    return response.data.datasetId
+    return response.data.datasetId;
+  }
+
+  static async inventory(datasetId) {
+    const vehicleIds = await VehicleApi.index(datasetId);
+    const dealerAndVehicleData = await this.#dealerInventory(
+      datasetId,
+      vehicleIds
+    );
+
+    return this.#normalizeDealerInventory(dealerAndVehicleData);
+  }
+
+  static async #dealerInventory(datasetId, vehicleIds) {
+    const promises = [];
+    for (const vehicleId of vehicleIds) {
+      promises.push(
+        new Promise(async (accept) => {
+          const vehicle = await VehicleApi.show(datasetId, vehicleId);
+          const dealer = await DealerApi.show(datasetId, vehicle.dealerId);
+          accept({
+            vehicle,
+            dealer,
+          });
+        })
+      );
+    }
+    return await Promise.all(promises);
+  }
+
+  static async #normalizeDealerInventory(dealerAndVehicleData) {
+    const dealers = {};
+    dealerAndVehicleData.forEach(({ vehicle, dealer }) => {
+      dealers[dealer.dealerId] ||= {
+        ...dealer,
+        vehicles: [],
+      };
+      dealers[dealer.dealerId].vehicles.push(vehicle);
+    });
+
+    return { dealers: Object.values(dealers) };
   }
 }
--- src/spec/dataset.spec.js
+++ src/spec/dataset.spec.js
@@ -8,4 +8,23 @@ describe("DatasetApi", () => {
       expect(datasetId.length).toBe(11);
     });
   });
+
+  describe(".inventory", () => {
+    it.only("returns the inventory of a particualr dataset", async () => {
+      const datasetId = await DatasetApi.getId();
+      const result = await DatasetApi.inventory(datasetId);
+      expect(result.dealers.constructor).toBe(Array);
+      expect(result.dealers.length).toBe(3);
+      result.dealers.forEach((dealer) => {
+        expect(typeof dealer.dealerId).toBe("number");
+        expect(typeof dealer.name).toBe("string");
+        expect(dealer.vehicles.length).toBe(3);
+        dealer.vehicles.forEach((vehicle) => {
+          expect(typeof vehicle.make).toBe("string");
+          expect(typeof vehicle.model).toBe("string");
+          expect(typeof vehicle.year).toBe("number");
+        });
+      });
+    });
+  });
 });
